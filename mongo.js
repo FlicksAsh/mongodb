d9e4d63eeb27d9ac7087f1a18391088819dc5a0f
nin bond <ianbond759@gmail.com>
2020-10-24 09:53:14 -0400

    added three more documents to books collections using db.books.insertMany([]) command

--- mongo.js
+++ mongo.js
@@ -25,3 +25,27 @@ db.books.insert({
       {"name": "Ned Stark"}
     ]
   })
+
+  db.books.insertMany([
+    {
+      "name": "Confident Ruby",
+      "publishedDate": new Date(),
+      "authors": [
+        { "name": "Avdi Grimm" }
+      ]
+    },
+    {
+      "name": "The War of Art",
+      "publishedDate": new Date(),
+      "authors": [
+        {"name": "Steven Pressfield"}
+      ]
+    },
+    {
+      "name": "Blink",
+      "publishedDate": new Date(),
+      "authors": [
+        {"name": "Malcolm Gladwell"}
+      ]
+    }
+  ])
